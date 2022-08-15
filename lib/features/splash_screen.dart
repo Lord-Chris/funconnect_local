d6fd6539f0a9532fe793b3bb651b4cff83ecd9e6
Mariam Hamzat <titilayobolamide247@gmail.com>
2022-08-15 19:35:21 +0100

    fixed some glitch in the splash file

--- lib/features/splash_screen.dart
+++ lib/features/splash_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:funconnect/constants/colors.dart';
+import 'package:funconnect/constants/fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   static const routeName = '/splash';
@@ -18,49 +19,64 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: AppColors.primary,
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Stack(
-                  alignment: AlignmentDirectional.topCenter,
-                  children: [
-                    Positioned(
-                      child:
-                          SvgPicture.asset("assets/images/splash_up_curve.svg"),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            const Spacer(),
-            Column(
-
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  "assets/icons/funconnect_logo.svg",
-                ),
-              ],
-            ),
-            const Spacer(),
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child:
-                      SvgPicture.asset("assets/images/splash_down_curve.svg"),
-                ),
-              ],
-            ),
-          ],
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: AppColors.primary,
+          child: Column(
+            children: [
+              Stack(
+                alignment: AlignmentDirectional.topCenter,
+                children: [
+                  Positioned(
+                    child: SvgPicture.asset(
+                      "assets/images/splash_up_curve.svg",
+                      width: 331,
+                      height: 331,
+                    ),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/icons/funconnect_logo.svg",
+                    width: 136,
+                    height: 24,
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Stack(
+                children: [
+                  SvgPicture.asset(
+                    "assets/images/splash_down_curve.svg",
+                    width: 331,
+                    height: 331,
+                  ),
+                  const Positioned(
+                    bottom: 40,
+                    left: 180,
+                    child: Text(
+                      "Funconnect Technologies",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w300,
+                        height: 1.3,
+                        fontFamily: AppFonts.gtWalshPro,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-
- 
 }
