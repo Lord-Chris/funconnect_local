de042293bbc969ab8635451988879180a67a684f
Lord-Chris <maduekechris65@gmail.com>
2022-08-25 11:46:22 +0100

    sign in view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:funconnect/welcome_view.dart';
+
+import 'email_signin_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const WelcomeView(),
+      home: const EmailSigninView(),
     );
   }
 }
