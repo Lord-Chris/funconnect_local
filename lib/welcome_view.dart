7d7633aee3a1a733eb2eeadbb9fb975a1619c9cf
Lord-Chris <maduekechris65@gmail.com>
2022-08-25 10:11:24 +0100

    WIP: Welcome screen

--- lib/welcome_view.dart
+++ lib/welcome_view.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:funconnect/constants/colors.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({Key? key}) : super(key: key);
+  static const welcomeImage = "assets/images/sign_in_image.jpeg";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ,
+      body: Column(
+        children: [
+          Image.asset(
+            welcomeImage,
+            fit: BoxFit.fitWidth,
+          ),
+          const Spacer(),
+        ],
+      ),
+      bottomSheet: Container(
+        height: 600,
+        padding: const EdgeInsets.all(24),
+        decoration: const BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+        ),
+        child: Column(
+          children:  [
+            Text("Welcome Back"),
+            Text("Jump back in where you stopped"),
+            
+          ],
+        ),
+      ),
     );
   }
 }
