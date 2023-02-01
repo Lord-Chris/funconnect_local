d24bc03f320858023afbb84961fa5712983a1feb
Mariam Hamzat <titilayobolamide247@gmail.com>
2023-02-01 16:23:14 +0100

    splash screen fixed

--- lib/features/startup/presentation/views/splash_view.dart
+++ lib/features/startup/presentation/views/splash_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:funconnect/features/startup/presentation/blocs/splash_bloc/splash_bloc.dart';
 import 'package:funconnect/features/startup/presentation/blocs/splash_bloc/splash_event.dart';
 import 'package:funconnect/features/startup/presentation/blocs/splash_bloc/splash_state.dart';
@@ -22,50 +21,10 @@ class SplashView extends StatelessWidget {
         child: Scaffold(
           body: Container(
             width: double.maxFinite,
-            color: AppColors.primary,
+            color: AppColors.mediumBlack,
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // SvgPicture.asset(
-                //   AppAssets.splashUpSvg,
-                //   width: 370,
-                // ),
-                const Spacer(),
-                SvgPicture.asset(
-                  AppAssets.funconnectSvg,
-                  width: 136,
-                  height: 24,
-                ),
-                const Spacer(),
-                // Stack(
-                //   children: [
-                //     SvgPicture.asset(
-                //       AppAssets.splashDownSvg,
-                //       width: 370,
-                //       // width: 331,
-                //       // height: 331,
-                //     ),
-                //     const Positioned(
-                //       bottom: 40,
-                //       left: 0,
-                //       right: 0,
-                //       child: Center(
-                //         child: Text(
-                //           "Funconnect Technologies",
-                //           style: TextStyle(
-                //             color: Colors.white,
-                //             fontSize: 14,
-                //             fontWeight: FontWeight.w300,
-                //             height: 1.3,
-                //             fontFamily: AppFonts.gtWalshPro,
-                //             fontStyle: FontStyle.normal,
-                //           ),
-                //         ),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Image.asset("assets/gifs/funconnect_logo.gif")],
             ),
           ),
         ),
