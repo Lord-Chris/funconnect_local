577cbac4167b74249ab2191c887bf8ac01e27eb3
Crucial Junior <nich.otieno@gmail.com>
2023-05-30 17:56:36 +0300

    update

--- lib/features/startup/presentation/views/onboarding_view_new.dart
+++ lib/features/startup/presentation/views/onboarding_view_new.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:funconnect/features/startup/presentation/components/onboarding_item_widget.dart';
+import 'package:funconnect/features/startup/presentation/domain/entities/onboarding_item_model.dart';
+import 'package:funconnect/shared/constants/colors.dart';
 import 'package:story_view/story_view.dart';
 
 class OnboardingViewNew extends StatelessWidget {
@@ -7,6 +10,33 @@ class OnboardingViewNew extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final storyViewController = StoryController();
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+              child: StoryView(
+            controller: storyViewController,
+            indicatorColor: AppColors.primary,
+            storyItems: [
+              StoryItem(
+                  OnboardingItemWidget(
+                    onboardingItem: onboardingItems[0],
+                  ),
+                  duration: const Duration(seconds: 5)),
+              StoryItem(
+                  OnboardingItemWidget(
+                    onboardingItem: onboardingItems[1],
+                  ),
+                  duration: const Duration(seconds: 5)),
+              StoryItem(
+                  OnboardingItemWidget(
+                    onboardingItem: onboardingItems[2],
+                  ),
+                  duration: const Duration(seconds: 5)),
+            ],
+          ))
+        ],
+      ),
+    );
   }
 }
