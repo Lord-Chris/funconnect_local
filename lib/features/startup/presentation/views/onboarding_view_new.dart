6436f1d9340ed6ca9626b854fbfad236f67267cb
Crucial Junior <nich.otieno@gmail.com>
2023-05-30 13:37:11 +0300

    add storyview

---
+++ lib/features/startup/presentation/views/onboarding_view_new.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:story_view/story_view.dart';
+
+class OnboardingViewNew extends StatelessWidget {
+  const OnboardingViewNew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final storyViewController = StoryController();
+    return const Scaffold();
+  }
+}
