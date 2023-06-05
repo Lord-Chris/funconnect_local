73e4ea594c43f653ff86d7fdcf62d9b2d7066f63
Lord-Chris <maduekechris65@gmail.com>
2023-06-05 18:05:40 +0100

    Swapped out location package

--- lib/services/location_service/location_service.dart
+++ lib/services/location_service/location_service.dart
@@ -1,25 +1,27 @@
 import 'dart:async';
 
 import 'package:funconnect/core/app/locator.dart';
-import 'package:funconnect/core/constants/_constants.dart';
 import 'package:funconnect/core/models/_models.dart';
 import 'package:funconnect/services/local_storage_service/i_local_storage_service.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart' hide Location;
+import 'package:location/location.dart';
+// import 'package:geolocator/geolocator.dart';
 import 'package:logger/logger.dart';
 
+import '../../core/constants/_constants.dart';
 import 'i_location_service.dart';
 
 class LocationService extends ILocationService {
   final _logger = Logger();
   AppLocation? _location;
   final _localStorageService = locator<ILocalStorageService>();
+  Location location = Location();
 
   @override
   Future<bool> canGetLocation() async {
-    final res = await Geolocator.checkPermission();
-    return res == LocationPermission.always ||
-        res == LocationPermission.whileInUse;
+    final permission = await location.hasPermission();
+    return permission == PermissionStatus.granted ||
+        permission == PermissionStatus.grantedLimited;
   }
 
   @override
@@ -31,48 +33,41 @@ class LocationService extends ILocationService {
         throw const Failure("Application requires location access");
       }
 
-      Position? res;
-      try {
-        _logger.i("Getting current location");
-        res = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.best,
-          timeLimit: const Duration(seconds: 5),
-        );
-        await _localStorageService.write(
-          HiveKeys.appBoxId,
-          key: StorageKeys.userSavedLocation,
-          data: res.toJson(),
-        );
-      } on LocationServiceDisabledException {
+      LocationData? res;
+      _logger.i("Getting current location");
+      res = await location.getLocation().timeout(const Duration(seconds: 5));
+      if (res.latitude == null || res.longitude == null) {
         _logger.i("Getting Last location from Hive");
-        res = Position.fromMap(_localStorageService.read(
+        _location = AppLocation.fromMap(_localStorageService.read(
           HiveKeys.appBoxId,
           key: StorageKeys.userSavedLocation,
         ));
-
-        _logger.i("Received location from Hive is ${res.toJson()}");
-      } on TimeoutException {
-        _logger.i("Getting Last known location");
-        res = await Geolocator.getLastKnownPosition();
-        _logger.i("Last known location is $res");
-      }
-      if (res == null) return null;
-      _logger.d(res);
-      _logger.i("Getting place details");
-      final place = await placemarkFromCoordinates(res.latitude, res.longitude);
-      if (place.isEmpty) {
-        _location = AppLocation(lat: res.latitude, long: res.longitude);
+        _logger.i("Received location from Hive is $_location");
+        return _location;
       } else {
-        _location = AppLocation(
-          lat: res.latitude,
-          long: res.longitude,
-          address: "${place.first.name} ${place.first.thoroughfare}",
-          city: place.first.locality,
-          state: place.first.administrativeArea,
-          country: place.first.country,
+        _logger.d(res);
+        _logger.i("Getting place details");
+        final place =
+            await placemarkFromCoordinates(res.latitude!, res.longitude!);
+        if (place.isEmpty) {
+          _location = AppLocation(lat: res.latitude!, long: res.longitude!);
+        } else {
+          _location = AppLocation(
+            lat: res.latitude!,
+            long: res.longitude!,
+            address: "${place.first.name} ${place.first.thoroughfare}",
+            city: place.first.locality,
+            state: place.first.administrativeArea,
+            country: place.first.country,
+          );
+        }
+        await _localStorageService.write(
+          HiveKeys.appBoxId,
+          key: StorageKeys.userSavedLocation,
+          data: _location!.toMap(),
         );
+        return _location;
       }
-      return _location;
     } on Exception catch (e) {
       _logger.e(e);
       throw Failure("Unable to get location", extraData: e.toString());
@@ -84,9 +79,9 @@ class LocationService extends ILocationService {
     try {
       final locationAllowed = await canGetLocation();
       if (locationAllowed) return true;
-      final res = await Geolocator.requestPermission();
-      return res == LocationPermission.always ||
-          res == LocationPermission.whileInUse;
+      final res = await location.requestPermission();
+      return res == PermissionStatus.granted ||
+          res == PermissionStatus.grantedLimited;
     } on Exception catch (e) {
       _logger.e(e);
       return false;
