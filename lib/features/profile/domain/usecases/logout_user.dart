93a5cab4ae00af257276ae37f1b4a6844dd34f63
Lord-Chris <maduekechris65@gmail.com>
2023-07-06 18:49:05 +0100

    Google sign out addition to signout logic

--- lib/features/profile/domain/usecases/logout_user.dart
+++ lib/features/profile/domain/usecases/logout_user.dart
@@ -3,12 +3,15 @@ import 'package:funconnect/core/usecases/usecase.dart';
 import 'package:funconnect/features/profile/data/repository/i_profile_repository.dart';
 import 'package:funconnect/services/_services.dart';
 
+import '../../../authentication/domain/usecases/google_signin_usecase.dart';
+
 class LogoutUser with UseCases<void, NoParams> {
   final _profileRepository = locator<IProfileRepository>();
   final _notificationService = locator<INotificationService>();
 
   @override
   Future<void> call(NoParams params) async {
+    GoogleSignInUsecase().signOut();
     await _profileRepository.logout();
     await _notificationService.clearUser();
   }
